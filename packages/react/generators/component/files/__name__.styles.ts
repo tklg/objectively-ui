5c5f71c168a1527445f0e09ef205773f49141fad
Theodore Kluge <1696813+tklg@users.noreply.github.com>
2023-02-04 11:41:11 -0500

    not worth the trouble yet

--- packages/react/generators/component/files/__name__.styles.ts
+++
@@ -1,6 +0,0 @@
-import { css } from '@emotion/react'
-import { ColorTheme } from 'src/types/ColorTheme'
-
-export const <%= name %>Styles = (theme: ColorTheme) => css({
-
-})
--- packages/react/generators/component/files/__name__.tsx
+++
@@ -1,7 +0,0 @@
-import { forwardRef } from 'react';
-
-export const <%= capitalize(name) %> = forwardRef((props, ref) => {
-  return (
-
-  )
-})
